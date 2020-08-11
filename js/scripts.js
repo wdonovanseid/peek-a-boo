a9b3574edb35533d96ed727590ffc6d102d56ef5
William Donovan-Seid <wdstwo@live.com>
2020-08-11 09:54:04 -0700

    add aardvark clickable
    co-author: Drew Parker <drewparker91@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+  $(".clickable1").click(function() {
+    $("#aardvark-showing").toggle();
+    $("#aardvark-hidden").toggle();
+  });
 });
