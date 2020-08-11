1fb96eb37b8447469144587221fb3739ae5644a6
William Donovan-Seid <wdstwo@live.com>
2020-08-11 10:19:45 -0700

    messed around with slides and fades
    co-author: Drew Parker <drewparker91@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
   });
   $(".clickable1").click(function() {
-    $("#aardvark-showing").toggle();
-    $("#aardvark-hidden").toggle();
+    $("#aardvark-showing").slideToggle();
+    $("#aardvark-hidden").slideToggle();
   });
 });
+
